0307aad6bfab52da3bc55e47958b8267bbbb8e8b
muhammet fırat <92649431+muhammettfirat@users.noreply.github.com>
2022-06-04 13:15:29 +0300

    Update MY_DATALOGGER.ino

--- ısı_nem/MY_DATALOGGER.ino
+++ ısı_nem/MY_DATALOGGER.ino
@@ -1,24 +1,47 @@
-#include<Servo.h>
-Servo motor;
-Servo motor1;
-int aci;
- 
+#include <dht.h>
+
+dht DHT;
+dht DHT1;
+#define DHT22_PIN 5
+#define DHT221_PIN 6
 void setup() {
- motor.attach(3);
- motor1.attach(4);
+  // put your setup code here, to run once:
+ Serial.begin(2000000);
 }
- 
+
 void loop() {
  
-for(aci=0;aci<=180;aci+=20){
-motor.write(aci);
-motor1.write(aci);
-delay(100);
-}
-for(aci=180;aci>=0;aci-=20){
-motor.write(aci);
-motor1.write(aci);
-delay(100);
-}
+
+  
+    int chk = DHT.read22(DHT22_PIN);
+ int chk1 = DHT1.read22(DHT221_PIN);
+
+   
+  // put your main code here, to run repeatedly:
+double nem,sicaklik;
+  nem=  DHT.humidity;
+ sicaklik=DHT.temperature;
+
+ double nem1,sicaklik1;
+  nem1=  DHT1.humidity;
+ sicaklik1=DHT1.temperature;
  
+ Serial.print(sicaklik,1);
+   Serial.print("*");
+ //Serial.print(" \n");
+     Serial.print(nem,1);
+  // Serial.print("*");
+Serial.print("*");
+//Serial.print(" \n");
+
+Serial.print(sicaklik1,1);
+   Serial.print("*");
+ //Serial.print(" \n");
+     Serial.print(nem1,1);
+  // Serial.print("*");
+Serial.print("*");
+Serial.print(" \n");
+  
+  //  Serial.println();
+delay(1000);
 }
