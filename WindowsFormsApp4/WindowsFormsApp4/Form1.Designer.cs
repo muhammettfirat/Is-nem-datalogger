d7b375e9defca06c24cd4bd16ea3f2ef191a8e66
muhammet fırat <92649431+muhammettfirat@users.noreply.github.com>
2022-06-04 13:19:33 +0300

    Add files via upload

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(123, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(109, 27);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.button1.Location = new System.Drawing.Point(301, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "BAĞLAN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.button2.Location = new System.Drawing.Point(22, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "VERİ AL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.button3.Location = new System.Drawing.Point(197, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(203, 48);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "VERİ ALIMINI DURDUR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PORT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "NEM (%):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "SICAKLIK (° C) :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(177, 26);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(70, 25);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(177, 26);
+            this.textBox3.TabIndex = 10;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(196, 47);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(130, 26);
+            this.textBox4.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(196, 118);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(130, 26);
+            this.textBox5.TabIndex = 18;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.textBox8);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.groupBox1.Location = new System.Drawing.Point(12, 290);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(404, 235);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ODA 1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(196, 182);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(130, 26);
+            this.textBox8.TabIndex = 19;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.groupBox2.Location = new System.Drawing.Point(647, 15);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(268, 133);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "SAAT-TARİH";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.button4.Location = new System.Drawing.Point(474, 138);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 48);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "KAYDET";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 900000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(1180, 621);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 62);
+            this.button5.TabIndex = 25;
+            this.button5.Text = "ACK";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox3.Controls.Add(this.textBox9);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.textBox6);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.textBox7);
+            this.groupBox3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox3.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.groupBox3.Location = new System.Drawing.Point(647, 290);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(412, 244);
+            this.groupBox3.TabIndex = 26;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "ODA 2";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(237, 182);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(130, 26);
+            this.textBox9.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 19);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "NEM (%):";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(237, 111);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(130, 26);
+            this.textBox6.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 19);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "SICAKLIK (° C) :";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(237, 42);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(130, 26);
+            this.textBox7.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.Color.Fuchsia;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Snow;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        internal System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Media;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        SoundPlayer player = new SoundPlayer();//SoundPlayer nesnesi oluşturulur.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+            SoundPlayer player = new SoundPlayer();//SoundPlayer nesnesi oluşturulur.
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string x = comboBox1.SelectedItem.ToString();
+            serialPort1.PortName = x;
+            serialPort1.BaudRate = 2000000;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 1200;
+            string dataFromArduino = serialPort1.ReadExisting().ToString();
+            string[] dataTempHumid = dataFromArduino.Split('*');
+        //    int sicaklik = Convert.ToInt32(textBox4.Text);
+   decimal sicaklik = Convert.ToDecimal(dataTempHumid[0]);
+   decimal sicaklik1 = Convert.ToDecimal(dataTempHumid[2]);
+            if (sicaklik >250 )
+            {
+                groupBox1.BackColor = Color.Red;
+SoundPlayer player = new SoundPlayer();//SoundPlayer nesnesi oluşturulur.
+string path = Application.StartupPath.ToString() + "\\beep-10.wav"; // Ses dosyası Bin\\Debug içindeki yeri. Dosyayı Debug içinden aldım.
+player.SoundLocation = path;//Ses dosyasının yolunu player a yükledik.
+player.Play(); //Ses dosyasını çal.
+                textBox8.Text = "Sıcak";
+            }
+            else
+            {
+                groupBox1.BackColor = Color.GreenYellow;
+                textBox8.Clear();
+            }
+            if  (sicaklik <170)
+            {
+                groupBox1.BackColor = Color.Red;
+                SoundPlayer player = new SoundPlayer();//SoundPlayer nesnesi oluşturulur.
+                string path = Application.StartupPath.ToString() + "\\beep-10.wav"; // Ses dosyası Bin\\Debug içindeki yeri. Dosyayı Debug içinden aldım.
+                player.SoundLocation = path;//Ses dosyasının yolunu player a yükledik.
+                player.Play(); //Ses dosyasını çal.
+                textBox8.Text = "Soğuk";
+            }
+            else
+            {
+               groupBox1.BackColor = Color.GreenYellow;
+                textBox8.Clear();
+            }
+            if (sicaklik1 > 250)
+            {
+                groupBox3.BackColor = Color.Red;
+
+                SoundPlayer player = new SoundPlayer();//SoundPlayer nesnesi oluşturulur.
+                string path = Application.StartupPath.ToString() + "\\beep-10.wav"; // Ses dosyası Bin\\Debug içindeki yeri. Dosyayı Debug içinden aldım.
+                player.SoundLocation = path;//Ses dosyasının yolunu player a yükledik.
+                player.Play(); //Ses dosyasını çal.
+                textBox9.Text = "Sıcak";
+            }
+            else
+            {
+               groupBox3.BackColor = Color.GreenYellow;
+                textBox9.Clear();
+            }
+            if (sicaklik1 < 170)
+            {
+                groupBox3.BackColor = Color.Red;
+
+                SoundPlayer player = new SoundPlayer();//SoundPlayer nesnesi oluşturulur.
+                string path = Application.StartupPath.ToString() + "\\beep-10.wav"; // Ses dosyası Bin\\Debug içindeki yeri. Dosyayı Debug içinden aldım.
+                player.SoundLocation = path;//Ses dosyasının yolunu player a yükledik.
+                player.Play(); //Ses dosyasını çal.
+                textBox9.Text = "Soğuk";
+            }
+            else
+            {
+                groupBox3.BackColor = Color.GreenYellow;
+                textBox9.Clear();
+            }
+            textBox4.Text = dataFromArduino.Split('*')[0];
+            textBox5.Text = dataFromArduino.Split('*')[1];
+            textBox7.Text = dataFromArduino.Split('*')[2];
+            textBox6.Text = dataFromArduino.Split('*')[3];
+            //   textBox5.Text = dataTempHumid[1].ToString();
+            // label4.Text = dataFromArduino+"";  
+            //   decimal nem = Convert.ToDecimal(dataFromArduino.Split()[1]);
+            //    textBox5.Text = nem.ToString(); 
+            // label5.Text = dataFromArduino.Split(',')[0]+"%";
+            //  textBox5.Text = dataFromArduino.Split(',')[1]+"C";
+            serialPort1.DiscardInBuffer();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            textBox3.Text = DateTime.Now.ToLongTimeString();
+            textBox2.Text = DateTime.Today.ToLongDateString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            try
+            {
+                if (!serialPort1.IsOpen)
+                    serialPort1.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Seri Port Seçin!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+          //  serialPort1.DiscardInBuffer();
+            serialPort1.Close();
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+    
+        
+        }
+
+        
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            StreamWriter dosya = File.AppendText("C:\\deneme.txt");
+            //  dosya.Write(textBox2.Text+"--"+textBox3.Text+"--"+textBox4.Text+"--"+textBox1.Text+ "-\n");
+            dosya.Write(textBox3.Text + "--" + textBox2.Text + "--" + textBox4.Text + "--" + textBox5.Text+ "--" + textBox4.Text + "--" + textBox5.Text + "--" + textBox8.Text + "--" + textBox7.Text + "--" + textBox6.Text+ "--" + textBox9.Text+ "-\n");
+            dosya.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 10; i++) 
+            { player.Stop(); }
+            
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
